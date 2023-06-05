b7214be734c27c3d34b9601b0fe310d406b49926
siri543 <siris6089@gmail.com>
2023-06-05 18:28:37 -0400

    person added

---
+++ Person.java
@@ -0,0 +1,12 @@
+package institution;
+
+public class Person {
+	String Pname="Siri";
+	int Page=23;
+	String Pemail="siri@gmail.com";
+	String Pphone="5597129007";
+	float Pheight;
+	float Pweight;
+	
+
+}
